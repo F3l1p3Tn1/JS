c59264bb1b07e22020eb6f0d9202adae526904fc
Felipe <130841537+F3l1p3Tn1@users.noreply.github.com>
2023-07-24 22:48:18 +0900

    ex010

---
+++ aulas/ex008.js
@@ -0,0 +1,16 @@
+// ex008.js
+var vel = 60.5
+console.log(`A velocidade e de ${vel}km/h`)
+if (vel > 60) {
+    console.log('Ultrapassou a velocidade. Multa!')
+}
+console.log('Use o cinto de segurança!')
+
+// ex009.js
+var pais =  'Brasil'
+console.log(`Vivendo no(a) ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log(`Estrangeiro`)
+}
